6cc9a6b1e6222f135ce6eacca9d2ba02f5d04f81
Yakov-Gibner <gibner88@mail.ru>
2022-03-22 12:11:52 +0300

    Refactoring Step 1

--- Step1.py
+++ Step1.py
@@ -1,42 +1,25 @@
 import json
-import csv
 from datetime import datetime
 
-# list ids of Black MERCEDES-BENZ
+
 black_Merc = []
-
-# list time of views (ids of Black MERCEDES-BENZ)
-event_time = []
+events_time = []
 
 
-# open ads.json
 with open('ads.json', 'r') as json_file:
-    json_list = list(json_file)
+    for json_str in json_file:
+        result_ads = json.loads(json_str)
+        if result_ads["make"] == 'MERCEDES-BENZ' and result_ads["attributes"]['exteriorColor'] == 'BLACK':
+            black_Merc.append(result_ads["id"])
 
 
-# open views.json as list
 with open('views.json', 'r') as json_file_views:
-    json_list_views = list(json_file_views)
+    for json_str in json_file_views:
+        result_views = json.loads(json_str)
+        event_t = datetime.utcfromtimestamp(result_views['event']['time']).strftime('%Y-%m-%dT%H:%M:%SZ')
+        if result_views['ad']['id'] in black_Merc:
+            events_time.append(f"{result_views['ad']['id']}, {event_t} \n")
 
 
-# looking for ids of Black MERCEDES-BENZ and write them to the list "black_Merc"
-for json_str in json_list:
-    result = json.loads(json_str)
-    if result["make"] == 'MERCEDES-BENZ' and result["attributes"]['exteriorColor'] == 'BLACK':
-        black_Merc.append(result["id"])
-
-
-# looking for time of views (ids of Black MERCEDES-BENZ) and write them to the list "event_time"
-for json_str in json_list_views:
-    result_views = json.loads(json_str)
-    event_t = datetime.utcfromtimestamp(result_views['event']['time']).strftime('%Y-%m-%dT%H:%M:%SZ')
-    if result_views['ad']['id'] in black_Merc:
-        event_time.append(f"{result_views['ad']['id']}, {event_t} \n")
-
-
-# writing output of ids and time to csv-file
-with open('step1.csv', mode='w') as csv_file:
-    csv_file.writelines(event_time)
-
-
-print(event_time)
+with open('step1_output.csv', mode='w') as csv_file:
+    csv_file.writelines(events_time)
